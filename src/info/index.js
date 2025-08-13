7436ed639d86a8bc069d2a7ae71b64af60b5f75e
xcatx <xcatx796@gmail.com>
2025-08-13 16:53:55 +0800

    GTM

--- src/info/index.js
+++ src/info/index.js
@@ -22,11 +22,11 @@ export default {
         ["經&ensp;紀&ensp;人", "林閔鴻(107)基市字第00167號"],
         */
     ],
-    gtmCode: ["GTM-KQ46V36N"], // 可放置多個
+    gtmCode: ["GTM-5Z2KK4RX"], // 可放置多個
     recaptcha_site_key_v2: "6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz", //主1
-    //recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
+    recaptcha_site_key_v2: "6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC", //主2
     //recaptcha_site_key_v2: "6LdbrqAmAAAAAPj2D_6cBbflea1livK9Uud4FGmN", //主3
-    recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
+    //recaptcha_site_key: "6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s", // recaptcha v3
     recaptcha_user_token: "6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa",
     order: {
         title: "預約賞屋",
